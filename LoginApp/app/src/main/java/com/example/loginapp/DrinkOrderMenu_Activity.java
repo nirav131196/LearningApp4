022239bda5a96a7d51ce2731a7ae44b42ae63344
niravg9191 <niravgirnari91@gmail.com>
2023-03-10 19:02:10 +0530

    COMMIT ON 10/3/2023 7:01 PM

--- LoginApp/app/src/main/java/com/example/loginapp/DrinkOrderMenu_Activity.java
+++ LoginApp/app/src/main/java/com/example/loginapp/DrinkOrderMenu_Activity.java
@@ -7,12 +7,27 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.navigation.NavigationView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DrinkOrderMenu_Activity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -24,6 +39,8 @@ public class DrinkOrderMenu_Activity extends BaseActivity implements NavigationV
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_order_menu);
 
+        token();
+
         List<Product_Data> list = new ArrayList<>();
         list =getData();
         recyclerView =(RecyclerView) findViewById(R.id.product_data);
@@ -31,6 +48,138 @@ public class DrinkOrderMenu_Activity extends BaseActivity implements NavigationV
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(DrinkOrderMenu_Activity.this));
     }
+    private void token() {
+        String url = "https://admin.p9bistro.com/index.php/generate_auth_token";
+        Log.e("checklog", url + "");
+
+        StringRequest stringRequest =new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Log.e("checklog", response + "");
+                JSONObject jsonObject = null;
+                try
+                {
+                    jsonObject = new JSONObject(response);
+                    String access_token = jsonObject.getString("access_token");
+                    Log.e("ACCESSTOKEN", access_token);
+                    getProductData(access_token);
+
+                } catch (JSONException je) {
+                    Toast.makeText(DrinkOrderMenu_Activity.this, "Error 2 : " + je, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.e("checklog",error + "");
+                Toast.makeText(getApplicationContext(), "Timeout Error", Toast.LENGTH_LONG).show();
+
+            }
+        }){
+            @Override
+            public Map<String,String> getHeaders() throws AuthFailureError {
+                HashMap<String,String> headers = new HashMap<>();
+                headers.put("x-api-key","XABRTYUX@123YTUFGB");
+                return headers;
+            }
+        };
+        RequestQueue requestquese = Volley.newRequestQueue(getApplicationContext());
+        requestquese.add(stringRequest);
+    }
+    private void getProductData(String access_token)
+    {
+        try
+        {
+            String url = "https://admin.p9bistro.com/index.php/getCategoryListondeptIds?deptids=[1]";
+
+            JSONObject req = new JSONObject();
+            try
+            {
+                req.put("deptids",1);
+            }
+            catch(Exception ex)
+            {
+                showToast("Exception "+ex);
+            }
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url,req,new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response)
+                {
+                    try
+                    {
+                        if(response.getBoolean("status"))
+                        {
+                            String message = response.getString("message");
+                          //  showToast(message);
+
+                            JSONObject resobj = response.getJSONObject("data");
+
+                            String id = resobj.getString("id");
+                            String categoryId = resobj.getString("category_id");
+                            String departmentId = resobj.getString("department_id");
+                            String categoryTypeId = resobj.getString("category_type_id");
+                            String catName = resobj.getString("cat_name");
+                            String image = resobj.getString("image");
+                            String catRank = resobj.getString("cat_rank");
+                            String hasCategory = resobj.getString("has_category");
+                            String isActive = resobj.getString("is_active");
+                            String createdDate = resobj.getString("created_date");
+                            String updatedDate = resobj.getString("updated_date");
+
+                            showToast(resobj.getString("id"));
+
+                        //    Toast.makeText(getApplicationContext(),"Id "+id,Toast.LENGTH_LONG).show();
+
+                          /*  List<Product_Data> list = new ArrayList<>();
+
+                            list.add(new Product_Data(id,categoryId,departmentId,catName));
+                            showToast(id + categoryId + departmentId + categoryTypeId + catName + image + catRank + hasCategory + isActive + createdDate + updatedDate);*/
+
+                     //       Log.d("RESPONSE GOT : ", id + categoryId + departmentId + categoryTypeId + catName + image + catRank + hasCategory + isActive + createdDate + updatedDate);
+                        }
+                    }
+                    catch (JSONException ex)
+                    {
+                        ex.printStackTrace();
+                    }
+                 //   showToast("Details Fetched");
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error)
+                {
+                    showToast("Fail to get Response : "+error);
+                }
+            }){
+                /*@Override
+                protected Map<String,String>getParams()
+                {
+                    Map<String,String> params = new HashMap<String,String>();
+                    params.put("email",email_login);
+                    params.put("password",password_login);
+                    params.put("login_by", String.valueOf(5));
+                    return params;
+                }*/
+                @Override
+                public Map<String,String> getHeaders()throws AuthFailureError
+                {
+                    Map<String,String>params = new HashMap<String ,String>();
+                    params.put("authorization",access_token);
+                    params.put("Content-Type", "application/json");
+                    return params;
+                }
+            };
+            RequestQueue requestQueue =Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(request);
+        }
+        catch (Exception ex)
+        {
+            Toast.makeText(DrinkOrderMenu_Activity.this, "Error 3 : "+ex, Toast.LENGTH_SHORT).show();
+        }
+    }
+
     private List<Product_Data> getData()
     {
         List<Product_Data> list = new ArrayList<>();
